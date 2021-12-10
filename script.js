e467ded46cfbfa885a1104e0b08fa2b706ffdf5a
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-12-10 21:48:58 +0100

    operator function and html skeleton

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// add, subtract, multiply, divide
-
 function add(a, b) {
     return a + b;
 }
@@ -15,3 +13,27 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+        default:
+            return 'Invalid Operator';
+            break;
+    }
+}
+
+// some numbers ... an operator ... some numbers ... equals/another operator ... repeat
+// after a calculation, store result in variable 1
+// on reset, clear both variables (undefined? not 0?)
